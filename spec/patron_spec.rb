17d01ed34ce72c923b970c7f1c49c0c14dc0eea1
Harrison Ryan <hwryan12@gmail.com>
2023-01-03 10:15:50 -0600

    Pass attributes test; Patron

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe Patron do
     it 'exists' do
       expect(patron_1).to be_instance_of(Patron)
     end
+
+    it 'has readable attributes' do
+      expect(patron_1.name).to eq("Bob")
+      expect(patron_1.spending_money).to eq(20)
+      expect(patron_1.interests).to eq([])
+    end
   end
 end
