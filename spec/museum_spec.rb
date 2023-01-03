e0a6ca0466ee3ad8b27dfdd363d24ff1694bb857
Harrison Ryan <hwryan12@gmail.com>
2023-01-03 10:22:46 -0600

    Pass #exists test; Museum

---
+++ spec/museum_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
+
+RSpec.describe Museum do
+  let(:dmns) { Museum.new("Denver Museum of Nature and Science") }
+  
+  describe '#initialize' do
+    it 'exists' do
+      expect(dmns).to be_instance_of(Museum)
+    end
+  end
+end
