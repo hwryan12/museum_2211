e9bfcf488d9ea1174220f8bc80510fc67647151f
Harrison Ryan <hwryan12@gmail.com>
2023-01-03 10:05:12 -0600

    Initial commit

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,5 +2,11 @@ require 'rspec'
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
-  
+  let(:exhibit) { Exhibit.new }
+
+  describe '#initialize' do
+    it 'exist' do
+      expect(exhibit).to be_instance_of(Exhibit)  
+    end
+  end
 end
