62a9a6ca6021dd97df65e74f0ce7573299eaddc1
Harrison Ryan <hwryan12@gmail.com>
2023-01-03 10:24:51 -0600

    Pass #attributes test; Museum

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -10,5 +10,10 @@ RSpec.describe Museum do
     it 'exists' do
       expect(dmns).to be_instance_of(Museum)
     end
+
+    it 'has readable attributes' do
+      expect(dmns.name).to eq("Denver Museum of Nature and Science") 
+      expect(dmns.exhibits).to eq([]) 
+    end
   end
 end
